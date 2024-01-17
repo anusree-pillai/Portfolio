6dd72aeb52c553a4a90c4e59735b4ae7d6831181
Anusree <anusree.pillai@gmail.com>
2024-01-17 15:42:33 +0100

    commiting models

--- my_portfolio/portfolio_projects/models.py
+++ my_portfolio/portfolio_projects/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    url = models.URLField()
+    
+    def __str__(self) :
+        return self.title
+
+class Skill(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name 
+
+class Education(models.Model):
+    degree = models.CharField(max_length=100) 
+    institution = models.CharField(max_length=500)
+    graduation_year = models.DateField()
+
+    def __str__(self):
+        return f"{self.degree} from {self.institution} ({self.graduation_year})"  
+    
+class Workexperience(models.Model):
+    job_title = models.CharField(max_length=100)
+    company = models.CharField(max_length=150)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    description = models.TextField()
+
+    def __self__(self):
+        return f"{self.job_title} at {self.company}"
+    
+class about(models.Model):
+    description = models.CharField(max_length=500)
+    resume_url = models.URLField()
+    image = models.ImageField()
+
+    def __self__(self):
+        return self.description,self.resume_url,self.image
+        
+
+
+    
+
+
+
+
+
+
