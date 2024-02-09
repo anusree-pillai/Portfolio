81dac5cbb31131737d7560b9f408bfb23841e980
Anusree <anusree.pillai@gmail.com>
2024-02-09 11:45:02 +0100

    committing portfolio changes

--- my_portfolio/my_portfolio/settings.py
+++ my_portfolio/my_portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portfolio_projects',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'my_portfolio.context_processors.project_context',
             ],
         },
     },
@@ -116,9 +119,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media')
+]
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
--- my_portfolio/portfolio_projects/views.py
+++ my_portfolio/portfolio_projects/views.py
@@ -1,3 +1,58 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.contrib import messages
+from .models import UserProfile,Portfolio,Education,Workexperience,Certificate
+from .forms import ContactForm
+from django.views import generic
 
-# Create your views here.
+class  IndexView(generic.TemplateView):
+    template_name = "portfolio_projects/index.html"
+    def get_context_data(self, **kwargs):
+        #return super().get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
+        certificates = Certificate.objects.filter(is_active=True)
+        portfolios = Portfolio.objects.filter(is_active=True)
+        education = Education.objects.filter(is_active=True)
+        experience = Workexperience.objects.filter(is_active=True)
+
+        context["certificates"] = certificates
+        context["portfolio"] = portfolios
+        context["education"] = education
+        context["experience"] = experience
+        return context
+    
+class ContactView(generic.FormView):
+    template_name = "portfolio_projects/contact.html"
+    form_class = ContactForm   
+    success_url= "/"
+
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request, 'Thank you')
+        return super().form_valid(form)
+    
+class PortfolioView(generic.ListView):
+    model = Portfolio
+    template_name = "portfolio_projects/portfolio.html"
+    paginate_by = 10
+
+    def get_queryset(self) :
+        return super().get_queryset().filter(is_active=True)
+    
+    
+class PortfolioDetailView(generic.DetailView):
+    model = Portfolio
+    template_name = "portfolio_projects/portfolio-detail.html"
+
+#class EducationView(generic.DetailView):
+class WorkExperineceView(generic.ListView):
+    model = Workexperience
+    template_name = "portfolio_projects/experience.html"
+    def get_queryset(self):
+        print(QuerySet)
+        return super().get_queryset().filter(is_active=True)
+
+class WorkExperienceDetailView(generic.DetailView):
+    model = Workexperience
+    template_name = "portfolio_projects/experience-detail.html"
